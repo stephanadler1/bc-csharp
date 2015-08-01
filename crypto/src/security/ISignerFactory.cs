0c7da771e796f56f5afbb4271bad113b36d1bdfa
Stephan Adler <dev@stephanadler.com>
2015-07-31 20:49:43 -0700

    Refactor the way ISigner instances are being created in order to allow ISigner implemenations to be replaced.
    - Refactoring SignerUtilities.GetSigner into separate class that implements ISignerFactory interface.
    - Provide the default factory as a property on SignerUtilities.
    - Set the factory to the default factory to ensure consistent behavior.

---
+++ crypto/src/security/ISignerFactory.cs
@@ -0,0 +1,13 @@
+﻿using Org.BouncyCastle.Crypto;
+
+namespace Org.BouncyCastle.Security
+{
+    /// <summary>
+    /// An extensible factory class that allows do overwrite or replace <see cref="ISigner"/>
+    /// implementations.
+    /// </summary>
+    public interface ISignerFactory
+    {
+        ISigner GetSigner(string algorithm);
+    }
+}
